eaffb3e71935f041dbf6daa8c1993554f44330f5
yannistannier <tannier.yannis@gmail.com>
2018-12-20 11:55:59 +0100

    add infogan

--- dcgan.py
+++ dcgan.py
@@ -89,7 +89,7 @@ class DCGAN():
         model.add(Activation('relu'))
         model.add(MaxPooling2D(pool_size=(2,2)))
         model.add(Flatten())
-        model.add(Dense(256, activation = "relu"))
+        model.add(Dense(128, activation = "relu"))
         model.add(Dropout(0.5))
         model.add(Dense(1))
         model.add(Activation('sigmoid'))
